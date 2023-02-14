d7c86d75524669ae66e26cdcf4ec423ecb443313
Peter Reutemann <fracpete@gmail.com>
2023-02-14 14:04:48 +1300

    output type renamed to conversion

--- src/spy_utils/envi_to_mat.py
+++ src/spy_utils/envi_to_mat.py
@@ -8,12 +8,12 @@ import spectral.io.envi as envi
 from spy_utils.core import Range, is_int
 
 
-OUTPUT_TYPE_SIMPLE = "simple"
-OUTPUT_TYPE_HAPPY = "happy"
+CONVERSION_SIMPLE = "simple"
+CONVERSION_HAPPY = "happy"
 
-OUTPUT_TYPES = [
-    OUTPUT_TYPE_SIMPLE,
-    OUTPUT_TYPE_HAPPY,
+CONVERSIONS = [
+    CONVERSION_SIMPLE,
+    CONVERSION_HAPPY,
 ]
 
 
@@ -52,7 +52,7 @@ def spectral_subset(img, bands=None):
     return b
 
 
-def output_simple(envi_input, img, mat_output, bands=None):
+def to_simple(envi_input, img, mat_output, bands=None):
     """
     Outputs the hyper-spectral data in a simple format: filename, bands, wave_length.
 
@@ -72,7 +72,7 @@ def output_simple(envi_input, img, mat_output, bands=None):
     scipy.io.savemat(mat_output, mdict=data)
 
 
-def output_happy(envi_input, img, mat_output, bands=None):
+def to_happy(envi_input, img, mat_output, bands=None):
     """
     Outputs the hyper-spectral data in happy format: FinalMask, ceiling, class, lambda, normcube, y.
 
@@ -109,7 +109,7 @@ def output_happy(envi_input, img, mat_output, bands=None):
     scipy.io.savemat(mat_output, mdict=data)
 
 
-def output(envi_input, mat_output, bands=None, output_type=OUTPUT_TYPE_SIMPLE):
+def convert(envi_input, mat_output, bands=None, conversion=CONVERSION_SIMPLE):
     """
     Converts the ENVI file to a Matlab file.
 
@@ -119,18 +119,20 @@ def output(envi_input, mat_output, bands=None, output_type=OUTPUT_TYPE_SIMPLE):
     :type mat_output: str
     :param bands: the range of 1-based band indices to output, uses all if None
     :type bands: str
-    :param output_type: how to output the hyperspectral data
-    :type output_type: str
+    :param conversion: how to convert the hyper-spectral data
+    :type conversion: str
     """
     img = envi.open(envi_input)
     if bands is None:
         bands = "first-last"
     bands = Range(bands, maximum=img.nbands).indices(zero_based=True)
 
-    if output_type == OUTPUT_TYPE_SIMPLE:
-        output_happy(envi_input, img, mat_output, bands)
+    if conversion == CONVERSION_SIMPLE:
+        to_simple(envi_input, img, mat_output, bands)
+    elif conversion == CONVERSION_HAPPY:
+        to_happy(envi_input, img, mat_output, bands)
     else:
-        raise Exception("Unhandled output type: %s" % output_type)
+        raise Exception("Unhandled conversion: %s" % conversion)
 
 
 def main(args=None):
@@ -147,9 +149,9 @@ def main(args=None):
     parser.add_argument("-i", "--input", metavar="FILE", help="the ENVI file to convert", required=True)
     parser.add_argument("-o", "--output", metavar="FILE", help="the RGB file to generate (JPG or PNG).", required=True)
     parser.add_argument("-b", "--bands", metavar="BANDS", help="the range of 1-based band indices to output; uses all bands if not specified.", default="first-last")
-    parser.add_argument("-t", "--output_type", choices=OUTPUT_TYPES, help="the type of Matlab file to generate.", default=OUTPUT_TYPE_SIMPLE)
+    parser.add_argument("-c", "--conversion", choices=CONVERSIONS, help="the conversion type to apply.", default=CONVERSION_SIMPLE)
     parsed = parser.parse_args(args=args)
-    output(parsed.input, parsed.output, bands=parsed.bands)
+    convert(parsed.input, parsed.output, bands=parsed.bands, conversion=parsed.conversion)
 
 
 def sys_main() -> int:
