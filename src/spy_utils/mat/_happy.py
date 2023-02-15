f8e0f322bb2ed4349e2582794837069dee83e9f1
Peter Reutemann <fracpete@gmail.com>
2023-02-15 13:23:31 +1300

    tweaked class

--- src/spy_utils/mat/_happy.py
+++ src/spy_utils/mat/_happy.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import scipy.io
 
@@ -27,19 +26,21 @@ def envi_to_happy(envi_input, img, mat_output, bands=None):
 
     envi_base = os.path.basename(os.path.dirname(envi_input))
     y_str = ""
+    label = ""
     for i in range(len(envi_base)):
         if envi_base[i] == ".":
             y_str += "."
         elif is_int(envi_base[i]):
             y_str += envi_base[i]
         else:
+            label = envi_base[i]
             break
     y = float(y_str)
 
     data = {
         "FinalMask": [1.0],  # TODO
         "ceiling": ceiling,
-        "class": True,  # TODO
+        "class": label == "r",  # x is False
         "normcube": normcube,
         "lambda": wavelength_subset(img, bands),
         "y": y,
